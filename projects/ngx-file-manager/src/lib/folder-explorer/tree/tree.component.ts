46bdfed2255e89321e0a2fdd68ef86580da40aeb
FabianoL <fabiano.lecce@acs.it>
2020-03-22 19:09:39 +0100

    Folder tree upload added

--- projects/ngx-file-manager/src/lib/folder-explorer/tree/tree.component.ts
+++ projects/ngx-file-manager/src/lib/folder-explorer/tree/tree.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import { Subject, Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { TreeService } from '../tree.service';
-
-const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'tree',
@@ -13,12 +11,16 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 export class TreeComponent implements OnInit, OnDestroy {
   @Input() parentId: string;
   @Input() dataList: any[];
+  @Input() uploadUrl: string;
+
+  uploaders: FileUploader[] = [];
 
   subscription: Subscription;
   selected: any;
   hasAnotherDropZoneOver: boolean;
 
-  constructor(private treeService: TreeService) {
+  constructor(
+    private treeService: TreeService) {
     this.subscription = treeService.select$.subscribe(msg => {
       this.dataList.forEach(x => {
         if (msg.node.item.id !== x.id) {
@@ -29,27 +31,34 @@ export class TreeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    for (const item of this.dataList) {
+      item.uploader = new FileUploader({
+        url: this.uploadUrl,
+        autoUpload: true
+      });
+    }
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  private createUploader(): FileUploader {
+  createUploader(): FileUploader {
     return new FileUploader({
-      url: URL,
-      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-      formatDataFunctionIsAsync: true,
-      formatDataFunction: async (item) => {
-        return new Promise((resolve, reject) => {
-          resolve({
-            name: item._file.name,
-            length: item._file.size,
-            contentType: item._file.type,
-            date: new Date()
-          });
-        });
-      }
+      url: this.uploadUrl,
+      autoUpload: true,
+      // disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      // formatDataFunctionIsAsync: true,
+      // formatDataFunction: async (item) => {
+      //   return new Promise((resolve, reject) => {
+      //     resolve({
+      //       name: item._file.name,
+      //       length: item._file.size,
+      //       contentType: item._file.type,
+      //       date: new Date()
+      //     });
+      //   });
+      // }
     });
   }
 
@@ -66,15 +75,15 @@ export class TreeComponent implements OnInit, OnDestroy {
     return this.dataList.find(x => x.parent === item.id);
   }
 
-  fileOver(event: any, item: any): void {
-    item.hasAnotherDropZoneOver = event;
-  }
-
   select(event, item): void {
     item.selected = !item.selected;
     this.treeService.select({ item });
 
     event.stopPropagation();
+  }
+
+  fileOver(event: any, item: any): void {
+    item.hasAnotherDropZoneOver = event;
   }
 
 }
